54542179f8d369a8b22949d0df1f08160fefffb4
jonathan-crepeau <jon.crepeau@gmail.com>
2020-01-28 20:45:45 -0800

    add HTML routes

--- server.js
+++ server.js
@@ -11,11 +11,38 @@ const PORT = process.env.PORT || 5000;
 
 // HTML Routes ================================== //
 
+// ANCHOR - Root Route
+
 app.get('/', (request, response) => {
     response.send(`
     <h1>Servin' Face Root Route</h1>
-    `)
-})
+    `);
+});
+
+// ANCHOR - Signup Route
+
+app.get('/signup', (request, response) => {
+    response.send(`
+    <h1>Signup Route</h1>
+    `);
+});
+
+// ANCHOR Login Route
+
+app.get('/login', (request, response) => {
+    response.send(`
+    <h1>Login Route</h1>
+    `);
+});
+
+// ANCHOR - Profile Route
+
+app.get('/profile', (request, response) => {
+    response.send(`
+    <h1>Profile Route</h1>
+    `);
+});
+
 
 // API Routes ================================== //
 
